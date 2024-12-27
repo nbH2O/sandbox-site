46b9725e271feb642e07fdecc85fc8f398c9936e
H2O <72228076+nbH2O@users.noreply.github.com>
2024-12-26 23:20:37 -0500

    first commit

---
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -0,0 +1,96 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->foreignId('role_id')->nullable()->constrained(
+                table: 'user_roles', indexName: 'role_id'
+            );
+
+            $table->text('description')->nullable();
+            $table->boolean('is_description_scrubbed');
+            $table->integer('currency');
+            $table->integer('points');
+            $table->tinyInteger('theme'); // 1 light 0 dark
+            $table->foreignId('primary_group_id')->nullable()->constrained(
+                table: 'groups', indexName: 'primary_group_id'
+            );
+            $table->uuid('avatar_hash');
+
+            $table->rememberToken();
+            $table->timestamp('born_at');
+            $table->timestamp('rewarded_at');
+            $table->timestamp('online_at');
+            $table->timestamps();
+        });
+
+        Schema::create('user_roles', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->text('description');
+            $table->string('icon');
+            $table->string('color'); // hex
+            $table->integer('power'); // can be null for random roles - but 'admin' should be higher than 'tester' for instance
+            $table->boolean('is_visible'); // on profile/comment/forum/etc
+        });
+
+        Schema::create('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        Schema::create('user_ip_logs', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->constrained();
+            $table->ipAddress();
+        });
+
+        Schema::create('user_friendships', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('sender_id')->nullable()->constrained(
+                table: 'users', indexName: 'sender_id'
+            );
+            $table->foreignId('receiver_id')->nullable()->constrained(
+                table: 'users', indexName: 'receiver_id'
+            );
+            $table->boolean('is_accepted');
+        });
+
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('user_roles');
+        Schema::dropIfExists('user_ip_logs');
+        Schema::dropIfExists('user_friendsships');
+        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('sessions');
+    }
+};
---
+++ database/migrations/2024_12_27_034446_create_site_tables.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('feature_statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_enabled');
+            $table->text('message')->nullable();
+        });
+
+        Schema::create('site_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('icon');
+            $table->string('color');
+            $table->string('type');
+            $table->text('message');
+        });
+
+        Schema::create('site_maintenance', function (Blueprint $table) {
+            $table->id()->unique(); // always ensure is 1 so table is constrained to single row
+            $table->text('message')->nullable();
+            $table->boolean('is_bypassable');
+            $table->integer('bypass_minimum_power'); // minimum role power to bypass the maintenance - if bypassable
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('feature_statuses');
+        Schema::dropIfExists('site_messages');
+        Schema::dropIfExists('site_maintenance');
+    }
+};
