30d6d9890edc4b83497b08ef7cd7130c9fbaada9
H2O <72228076+nbH2O@users.noreply.github.com>
2025-02-15 17:05:37 -0500

    Login page glowup + login help modal scaffolding

--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -4,11 +4,13 @@
         // this is really only for login dropdown on header
         'md' => [
             'form' => 'gap-2',
-            'error' => 'text-xs font-bold block'
+            'error' => 'text-xs font-bold block',
+            'extras' => false
         ],
         'lg' => [
             'form' => 'gap-4',
-            'error' => 'text-sm'
+            'error' => 'text-sm',
+            'extras' => true
         ]
     ]
 ])
@@ -36,6 +38,36 @@
         @error('general')
             <span class="text-red {{ $sizeVals[$size]['error'] }}">{{ $message }}</span>
         @enderror
-        <x-button x-on:click="$wire.submit()" wire:loading.attr="data-busy" color="green" size="{{ $size }}">{{ __('Submit') }}</x-button>
+        <x-button class="w-full" x-on:click.prevent="$wire.submit()" wire:loading.attr="data-busy" color="green" size="{{ $size }}">{{ __('Submit') }}</x-button>
+    </div>
+    <div>
+        @if ($sizeVals[$size]['extras'])
+            <p class="mb-2">{{ __('Looking for something else?') }}</p>
+        @endif
+        <div class="flex gap-4 [&>*]:grow">
+            @if ($sizeVals[$size]['extras'])
+                <x-button href="{{ route('register') }}" size="md" color="primary">{{ __('Join') }}</x-button>
+            @endif
+            <x-modal title="{{ __('Login Help') }}">
+                <x-slot name="trigger">
+                    <x-button class="w-full" x-on:click.prevent size="{{ $sizeVals[$size]['extras'] ? 'md' : 'sm' }}" color="{{ $sizeVals[$size]['extras'] ? 'gray' : 'transparent' }}">{{ __('Help') }}</x-button>
+                </x-slot>
+
+                <div class="flex flex-col gap-4">
+                    <div>
+                        <p>{{ __('Forgot Password?') }}</p>
+                        <x-button class="ms-auto" color="blue">{{ __('Recover Password') }}</x-button>
+                    </div>
+                    <div>
+                        <p>{{ __('Forgot Username?') }}</p>
+                        <x-button class="ms-auto" color="blue">{{ __('Recover Username') }}</x-button>
+                    </div>
+                    <div>
+                        <p>{{ __('Forgot Password?') }}</p>
+                        <x-button class="ms-auto" color="blue">{{ __('Contact Us') }}</x-button>
+                    </div>
+                </div>
+            </x-modal>
+        </div>
     </div>
 </form>
