ebfbbad28dd3a0643ce186b793a77a979e8f2121
H2O <72228076+nbH2O@users.noreply.github.com>
2025-02-23 16:56:21 -0500

    CheckMaintenanceStatus artisan command

---
+++ app/Console/Commands/CheckMaintenanceStatus.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\App;
+
+class CheckMaintenanceStatus extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:check-maintenance-status';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Check if the app is currently in hard maintenance mode';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if (App::isDownForMaintenance()) {
+            $this->info(true);
+        } else {
+            $this->info(false);
+        }
+    }
+}
