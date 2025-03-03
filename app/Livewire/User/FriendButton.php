a647de309a924ee06ddd2c6e0706567cd6336942
H2O <72228076+nbH2O@users.noreply.github.com>
2025-03-02 19:54:44 -0500

    Friend button fix 2

--- app/Livewire/User/FriendButton.php
+++ app/Livewire/User/FriendButton.php
@@ -30,7 +30,7 @@ class FriendButton extends Component
             $q->where('sender_id', $this->user->id)
                 ->orWhere('sender_id', $au->id);
         })
-        ->andWhere(function ($q) use ($au) {
+        ->where(function ($q) use ($au) {
             $q->where('receiver_id', $this->user->id)
                 ->orWhere('receiver_id', $au->id);
         })->first();
